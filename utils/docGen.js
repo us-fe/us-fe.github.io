804e334ef3d6e2337f38db3c9b9f3277fa5a74cc
juconghe <hejucong1995@gmail.com>
2022-04-22 22:35:23 -0400

    cleanup

--- utils/docGen.js
+++ utils/docGen.js
@@ -38,17 +38,14 @@ return template;
 }
 
 
-readline.question('Enter the filename: ', name => {
-    const filename = name;
-    let category;
+readline.question('Enter the filename: ', filename => {
     readline.question('Chose a category: 1: coding, 2: javascript, 3: react ', chose => {
-        category = chose;
         readline.question('Enter the title for the page ', title => {
             readline.question('Enter url of the question ', problemUrl => {
                 readline.close();
                 let problemName = 'coding';
-                if (category === '2') problemName = 'javascript';
-                if (category === '3') problemName = 'react';
+                if (chose === '2') problemName = 'javascript';
+                if (chose === '3') problemName = 'react';
                 try {
                     fs.writeFileSync(`docs/problems/${problemName}/${filename}.md`, getTemplate(title, problemUrl));
                 } catch (e) {
